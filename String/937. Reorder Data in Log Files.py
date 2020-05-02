09f3972087491d32a591448936e6aea6612cd8ac
Yathu Arulnanthy <yathu@outlook.com>
2020-05-02 17:12:28 +1000

    String: Added faster solution for Reorder log Files

--- String/937. Reorder Data in Log Files.py
+++ String/937. Reorder Data in Log Files.py
@@ -58,3 +58,14 @@ class Solution:
             res.append(digit)
         
         return res
+
+
+# A much faster solution
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        def f(log):
+            id, rest = log.split(" ", 1)
+            
+            return (0, rest, id) if rest[0].isalpha() else (1,)
+        
+        return sorted(logs, key=f)
