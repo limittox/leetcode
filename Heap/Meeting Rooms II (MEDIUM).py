9e1d8aa98d4c2f717c0cca8aab04d305ec456fbe
Yathu Arulnanthy <yathu@outlook.com>
2020-06-01 18:33:03 +1000

    Heap: Meeting rooms II added

--- Heap/Meeting Rooms II (MEDIUM).py
+++ Heap/Meeting Rooms II (MEDIUM).py
@@ -11,6 +11,7 @@ Example 2:
 Input: [[7,10],[2,4]]
 Output: 1
 NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+https://leetcode.com/problems/meeting-rooms-ii/solution/
 """
 
 import heapq
@@ -46,3 +47,24 @@ class Solution:
             heapq.heappush(heap, endTime)
             
         return len(heap)
+
+# O(nlogn) time and O(1) space
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        startTimes = [x[0] for x in intervals]
+        endTimes = [x[1] for x in intervals]
+        
+        startTimes.sort()
+        endTimes.sort()
+        
+        s_ptr, e_ptr = 0, 0
+        
+        rooms = 0
+        while s_ptr < len(startTimes):
+            if startTimes[s_ptr] >= endTimes[e_ptr]:
+                rooms -= 1
+                e_ptr += 1
+            rooms += 1
+            s_ptr += 1
+        return  rooms
